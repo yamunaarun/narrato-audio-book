067ec69f86ea9a42dc45a60ca780d2982b18a615
yamunaarun <yamunaarun26@gmail.com>
2026-02-19 12:39:25 +0530

    Updated favicon, index.html, package-lock, and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -11,6 +13,10 @@ export default defineConfig(({ mode }) => ({
     hmr: {
       overlay: false,
     },
+    // <-- Add this line inside server:
+    allowedHosts: [
+      ".ngrok-free.dev" // allow all free ngrok URLs
+    ],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
